057fe403084091992a16c6745160c60dc27257ce
Neil Vallecer <neilvallecer12@gmail.com>
2026-06-27 23:23:18 +0800

    refactor: extract shared app, simplify index.ts

---
+++ backend/src/core/app.ts
@@ -0,0 +1,19 @@
+import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
+import { authRoutes, authProtectedRoutes } from '../routes/authRoutes';
+import { eventRoutes } from '../routes/eventRoutes';
+import { calendarRoutes } from '../routes/calendarRoutes';
+import { profileRoutes } from '../routes/profileRoutes';
+import { successWrap } from '../plugins/successWrap';
+import { errorHandler } from '../plugins/errorHandler';
+
+export const app = new Elysia()
+  .use(cors())
+  .use(successWrap)
+  .use(errorHandler)
+  .use(authRoutes)
+  .use(authProtectedRoutes)
+  .use(eventRoutes)
+  .use(calendarRoutes)
+  .use(profileRoutes)
+  .get('/', () => 'Backend is running');
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,7 @@
 import 'dotenv/config';
-import { Elysia } from "elysia";
-import { cors } from "@elysiajs/cors";
-import './config/drizzle';
-import { authRoutes, authProtectedRoutes } from "./routes/authRoutes";
-import { eventRoutes } from "./routes/eventRoutes";
-import { calendarRoutes } from "./routes/calendarRoutes";
-import { profileRoutes } from "./routes/profileRoutes";
-import { successWrap } from "./plugins/successWrap";
-import { errorHandler } from "./plugins/errorHandler";
+import { app } from './core/app';
 
-const app = new Elysia()
-  .use(cors())
-  .use(successWrap)
-  .use(errorHandler)
-  .use(authRoutes)
-  .use(authProtectedRoutes)
-  .use(eventRoutes)
-  .use(calendarRoutes)
-  .use(profileRoutes)
-
-app.get("/", () => "Backend is running")
-
-app.listen(3000)
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
